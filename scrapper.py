9479f58c1d0a9bfdc95fa21bc3d0a80c7883fa98
Marcin <cukiernick@gmail.com>
2022-06-06 15:30:40 +0200

    dataclass, new parser

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,20 @@
 from typing import List
-from bs4 import BeautifulSoup
+
 import requests
+from bs4 import BeautifulSoup
+from pydantic import BaseModel
+from typing_extensions import Literal
+
+
+class ChannelData(BaseModel):
+    receiver_id: int
+    channel_id: int
+    lock_status: Literal["Locked", "Unlocked"]
+    frequency: int
+    modulation: str
+    symbol_rate: int
+    snr: float
+    power: float
 
 
 def scrap_downstream(html_down: str):
@@ -12,6 +26,17 @@ def scrap_downstream(html_down: str):
     for td in soup_down.find_all("td"):
         td_list.append(td.text)
     tdx_list = [x for idx, x in enumerate(td_list) if td_list[idx] != "\n\n"]
+
+    tbody = soup_down.find("tbody")
+    rows = tbody.find_all("tr")
+    for row in rows:
+        tds = row.find_all("td")
+        id = tds[0].text
+        channel = tds[1].text
+        rate = tds[5]
+        symbol_raw = rate.find("script").text
+        symbol_raw.lstrip('i18n("').rstrip('")')
+
 
     script_tags = soup_down.find_all("script")
     script_tags = script_tags[34:58]
